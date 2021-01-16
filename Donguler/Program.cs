d96069a9312a4934258370a01a9bbbdc74088479
Ensar KILIÇ <ensar.kilic@tedas.gov.tr>
2021-01-16 23:46:20 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici yetiştirme kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C#";
+
+
+            //array-dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici yetiştirme kampı", 
+                "Programlamaya başlangıç için temel kurs","Java", "Python", "C#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar) //kursları dolaş-dizilerde uygulanır dizileri tek tek 
+                //dolaşmaya yarar
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu- footer");
+        }
+    }
+}
